4dd482ae0ef56545b00820e90db1691bbd4b8c06
fisherb <billy.fisher@igt.com>
2015-10-14 18:52:26 -0700

    Improve build to deal with plain vanilla Ubuntu environments where bundleDependencies were found missing from npmbox package.json.

--- build-offline-tarball.sh
+++ build-offline-tarball.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+BOT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 rm -f npmbox*.tgz
-set -e  # Stop on first error
+rm -rf build-offline-tarball
+set -xev # Stop on first error
 
 #
 # Build Bundled Offline Tarball
 #
 mkdir build-offline-tarball
 cd build-offline-tarball
-npm install npmbox
-cd node_modules
-cd npmbox
+npmbox_location=$(npm install npmbox 2>&1 | grep "npmbox@" | cut -d' ' -f2)
+cp $BOT_SCRIPT_DIR/bundle_app.js $npmbox_location
+cd $npmbox_location
+npm install findit
+node bundle_app.js
 npm pack
-mv npmbox*.tgz ../../../
-cd ../../../
+mv npmbox*.tgz $BOT_SCRIPT_DIR
+cd $BOT_SCRIPT_DIR
 
 #
 # Cleanup
